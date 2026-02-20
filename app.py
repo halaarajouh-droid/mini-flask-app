c89d741fa0df9da14dcd5a13b69328ee5b8c08bd
halaarajouh-droid <halaarajouh@gmail.com>
2026-02-20 17:24:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-if if __name__=="__main__":
-    port=int(os.environ.get("PORT",10000))
-    app.run(host="0.0.0.0",port=port,debug=True)   
 
 
